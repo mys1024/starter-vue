866df3d689e123957adfbce7e0e0cf000b900b57
mys1024 <mys.1024@outlook.com>
2025-01-02 02:07:34 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import App from './App.vue';
 import 'ant-design-vue/dist/reset.css';
 import 'virtual:uno.css';
-import './assets/main.less';
+import './assets/main.scss';
 
 // vue app
 const app = createApp(App);
